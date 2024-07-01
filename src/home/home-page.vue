bfc31e91f81ef5a6d4632cf0899b86296fa8e378
Aezurly <66571923+Aezurly@users.noreply.github.com>
2024-07-01 11:19:19 -0400

    main home page component add

--- src/home/home-page.vue
+++ src/home/home-page.vue
@@ -1,15 +1,22 @@
 <script setup>
 import BaseHeader from '../../src/components/BaseHeader.vue'
+import HomeMain from '../../src/components/HomeMain.vue'
 </script>
 
 <template>
 
-
-  <BaseHeader />
+  <BaseHeader/>
 
   <main>
     <h1>Home</h1>
+    <HomeMain/>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "../assets/base.scss";
+
+main {
+}
+
+</style>
