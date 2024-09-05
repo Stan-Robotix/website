10a751af9c5e1594fed378da63f5bcca452e09b4
Timothee Laberge <timotheelaberge@gmail.com>
2024-09-05 12:22:34 -0400

    Created "about" page

---
+++ src/about/about-page.vue
@@ -0,0 +1,4 @@
+<script setup>
+import BaseHeader from '../../src/components/BaseHeader.vue'
+import BaseFooter from '../../src/components/BaseFooter.vue'
+</script>
---
+++ src/about/about.js
@@ -0,0 +1,6 @@
+import '../../src/assets/main.scss'
+
+import { createApp } from 'vue'
+import About from './about-page.vue'
+
+createApp(About).mount('#app')
