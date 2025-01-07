f8f27f761dc9d893ced5086452fee64d971289ae
Aezurly <66571923+Aezurly@users.noreply.github.com>
2025-01-06 20:13:03 -0500

    Add rollup-plugin-copy to package.json and configure Vite to copy PDF files to the dist directory

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: "pdf/*", dest: "dist/pdf" }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
